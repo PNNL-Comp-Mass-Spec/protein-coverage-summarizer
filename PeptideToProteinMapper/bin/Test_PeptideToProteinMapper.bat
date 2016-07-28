4a877dd5aeb9f420d53e37f443817cfbc7d15aac
alchemistmatt <matthew.monroe@pnnl.gov>
2016-07-27 17:53:00 -0700

    Add test files

---
+++ PeptideToProteinMapper/bin/Test_PeptideToProteinMapper.bat
@@ -0,0 +1,4 @@
+
+
+AnyCPU\PeptideToProteinMapper.exe QC_Shew_excerpt.txt /R:Shewanella_2006-07-11.fasta /F:1 /O:Mode1
+AnyCPU\PeptideToProteinMapper.exe QC_Shew_excerpt.txt /R:Shewanella_2006-07-11.fasta /F:4 /O:Mode4
