ef53b2447a844c74039a8cb4fb9b9c60208d752b
alchemistmatt <matthew.monroe@pnnl.gov>
2017-06-08 17:20:31 -0700

    Update dates

--- PeptideToProteinMapper/modMain.vb
+++ PeptideToProteinMapper/modMain.vb
@@ -17,18 +17,18 @@
 ' E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com
 ' Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/ or http://panomics.pnnl.gov/
 ' -------------------------------------------------------------------------------
-' 
+'
 ' Licensed under the Apache License, Version 2.0; you may not use this file except
-' in compliance with the License.  You may obtain a copy of the License at 
+' in compliance with the License.  You may obtain a copy of the License at
 ' http://www.apache.org/licenses/LICENSE-2.0
 '
-' Notice: This computer software was prepared by Battelle Memorial Institute, 
-' hereinafter the Contractor, under Contract No. DE-AC05-76RL0 1830 with the 
-' Department of Energy (DOE).  All rights in the computer software are reserved 
-' by DOE on behalf of the United States Government and the Contractor as 
-' provided in the Contract.  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY 
-' WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS 
-' SOFTWARE.  This notice including this sentence must appear on any copies of 
+' Notice: This computer software was prepared by Battelle Memorial Institute,
+' hereinafter the Contractor, under Contract No. DE-AC05-76RL0 1830 with the
+' Department of Energy (DOE).  All rights in the computer software are reserved
+' by DOE on behalf of the United States Government and the Contractor as
+' provided in the Contract.  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY
+' WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS
+' SOFTWARE.  This notice including this sentence must appear on any copies of
 ' this computer software.
 
 Imports System.IO
@@ -39,7 +39,7 @@ Imports PeptideToProteinMapEngine.clsPeptideToProteinMapEngine
 
 Public Module modMain
 
-    Public Const PROGRAM_DATE As String = "July 27, 2016"
+    Public Const PROGRAM_DATE As String = "June 8, 2017"
 
     Private mPeptideInputFilePath As String
     Private mProteinInputFilePath As String
@@ -215,7 +215,7 @@ Public Module modMain
         Dim intValue As Integer
 
         Try
-            ' Make sure no invalid parameters are present 
+            ' Make sure no invalid parameters are present
             If objParseCommandLine.InvalidParametersPresent(lstValidParameters) Then
                 ShowErrorMessage("Invalid commmand line parameters",
                   (From item In objParseCommandLine.InvalidParameters(lstValidParameters) Select "/" + item).ToList())
