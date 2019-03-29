3e7ca6b4030ae70893151cfd312798fbba2f6580
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-28 19:01:25 -0700

    Update the program date

--- modMain.vb
+++ modMain.vb
@@ -26,7 +26,7 @@ Imports PRISM
 ''' </summary>
 Public Module modMain
 
-    Public Const PROGRAM_DATE As String = "March 22, 2019"
+    Public Const PROGRAM_DATE As String = "March 28, 2019"
 
     Private mPeptideInputFilePath As String
     Private mProteinInputFilePath As String
