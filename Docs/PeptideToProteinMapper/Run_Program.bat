b52889940d1b178908351571665357d71122636c
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-30 13:39:37 -0700

    Add batch files

---
+++ Docs/PeptideToProteinMapper/Run_Program.bat
@@ -0,0 +1,4 @@
+..\..\PeptideToProteinMapper\bin\Debug\PeptideToProteinMapper.exe ..\QC_Mam_19_01_a_12Oct20_Pippin_Rep-20-08-08_msgfplus.tsv /O:. /R:..\M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /A
+..\..\PeptideToProteinMapper\bin\Debug\PeptideToProteinMapper.exe ..\ProteinAndPeptide.txt /O:. /R:..\M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /A
+..\..\PeptideToProteinMapper\bin\Debug\PeptideToProteinMapper.exe ..\PeptideOnly.txt       /O:. /R:..\M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /A
+pause
---
+++ Docs/Run_Program.bat
@@ -0,0 +1,5 @@
+..\bin\Debug\ProteinCoverageSummarizerGUI.exe QC_Mam_19_01_a_12Oct20_Pippin_Rep-20-08-08_msgfplus.tsv /R:M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /D
+..\bin\Debug\ProteinCoverageSummarizerGUI.exe ProteinAndPeptide.txt /R:M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /D
+..\bin\Debug\ProteinCoverageSummarizerGUI.exe PeptideOnly.txt       /R:M_musculus_Uniprot_SPROT_2013_09_2013-09-18_Tryp_Pig_Bov.fasta /D
+
+pause
