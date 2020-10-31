adf9714bd9308e27bfae4da4c4690cf010c40a94
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-30 17:41:43 -0700

    Expand the batch file that updates zip files

--- ProteinCoverageSummarizer_Installer/Zipped/Update_Zip_Files.bat
+++ ProteinCoverageSummarizer_Installer/Zipped/Update_Zip_Files.bat
@@ -1,7 +1,17 @@
+@echo off
+echo Be sure to build the program in Release mode
+pause
+
+@echo on
 "c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\README.md
 "c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\RevisionHistory.txt
-"c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\AnyCPU\*.exe
-"c:\Program Files\7-Zip\7z.exe" a -r PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\AnyCPU\*.dll
+"c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Release\*.exe
+"c:\Program Files\7-Zip\7z.exe" a -r PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Release\*.dll
+
+"c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\README.md
+"c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\RevisionHistory.txt
+"c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\bin\Release\*.exe
+"c:\Program Files\7-Zip\7z.exe" a -r ProteinCoverageSummarizer.zip ..\..\bin\Release\*.dll
 
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer_Installer.zip ..\..\README.md
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer_Installer.zip ..\..\RevisionHistory.txt
