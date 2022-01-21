2e3ae602713bb4abacd8ac4027b0289fc012191e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-21 15:44:17 -0800

    Update batch file to copy from the Debug build

--- ProteinCoverageSummarizer_Installer/Zipped/Update_Zip_Files.bat
+++ ProteinCoverageSummarizer_Installer/Zipped/Update_Zip_Files.bat
@@ -1,17 +1,17 @@
 @echo off
-echo Be sure to build the program in Release mode
+echo Be sure to build the program in Debug mode
 pause
 
 @echo on
 "c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\README.md
 "c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\RevisionHistory.txt
-"c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Release\*.exe
-"c:\Program Files\7-Zip\7z.exe" a -r PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Release\*.dll
+"c:\Program Files\7-Zip\7z.exe" a PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Debug\*.exe
+"c:\Program Files\7-Zip\7z.exe" a -r PeptideToProteinMapper.zip ..\..\PeptideToProteinMapper\bin\Debug\*.dll
 
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\README.md
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\RevisionHistory.txt
-"c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\bin\Release\*.exe
-"c:\Program Files\7-Zip\7z.exe" a -r ProteinCoverageSummarizer.zip ..\..\bin\Release\*.dll
+"c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer.zip ..\..\bin\Debug\*.exe
+"c:\Program Files\7-Zip\7z.exe" a -r ProteinCoverageSummarizer.zip ..\..\bin\Debug\*.dll
 
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer_Installer.zip ..\..\README.md
 "c:\Program Files\7-Zip\7z.exe" a ProteinCoverageSummarizer_Installer.zip ..\..\RevisionHistory.txt
