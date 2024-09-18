cbe39dca15a8e7c5b683a6d17b228dd3a31bae6e
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-17 20:27:25 -0700

    Update program date

--- PeptideToProteinMapper/Program.cs
+++ PeptideToProteinMapper/Program.cs
@@ -48,7 +48,7 @@ namespace PeptideToProteinMapper
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "July 22, 2024";
+        public const string PROGRAM_DATE = "September 17, 2024";
 
         private static string mParameterFilePath;
         private static string mInspectParameterFilePath;
